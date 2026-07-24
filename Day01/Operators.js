eb95ca1bf16cc93d22a8e181c4e9a4aeb7ae42c6
SATHYAMOORTHYTS01 <sathyamoorthyts0105@gmail.com>
2026-07-24 10:50:29 +0530

    Day2 Home Assignments

---
+++ Day01/Operators.js
@@ -0,0 +1,46 @@
+// Arithmetic Operators :
+
+//  Arithmetic operators are used in mathematical expressions in the same way that they are used in algebra.
+// The following table lists the arithmetic operators:
+//  Operator   		Result
+//  + 		Addition (also unary plus)
+//  – 		Subtraction (also unary minus)
+//  * 		Multiplication
+//  / 		Division
+//  % 		Modulus- REMAINDER
+
+
+// Assignment Details:
+// Declare variables using `let` for different data types in JavaScript, and verify their types using `typeof`,
+// including an uninitialized variable.
+
+
+a = 10
+b = 5
+
+c = a + b
+console.log(c);
+
+c = a-b
+console.log(c);
+
+c = a*b
+console.log(c);
+c = a/b
+console.log(c);
+c = a%b 
+console.log(c)
+
+
+//  ++ 		Increment
+//  += 		Addition assignment
+//  –= 		Subtraction assignment
+//  *= 		Multiplication assignment
+//  /= 		Division assignment
+//  %= 		Modulus assignment
+//  –– 		Decremen
+
+// a ++ b = increment
+
+a = ++ b
+console.log(a);
